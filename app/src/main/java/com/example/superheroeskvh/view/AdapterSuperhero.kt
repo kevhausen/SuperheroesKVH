c406bb5f68b96bf01641cf1df4bc0e43cd2fbb7b
kevin <kevin.vonahausen@gmail.com>
2020-08-30 20:47:17 -0400

    1.0 funciona!! sin emabrgo la url de la imagen esta hardcodeada en el viewholder y hay algunos metodos que se pueden simplificar

--- app/src/main/java/com/example/superheroeskvh/view/AdapterSuperhero.kt
+++ app/src/main/java/com/example/superheroeskvh/view/AdapterSuperhero.kt
@@ -1,5 +1,45 @@
 package com.example.superheroeskvh.view
 
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.superheroeskvh.R
+import com.example.superheroeskvh.model.dataclass.Superhero
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.superhero_viewholder.view.*
+
 //viewholder con id, item y foto chica
-class AdapterSuperhero {
+
+class AdapterSuperhero(var mDataset : List<Superhero>) : RecyclerView.Adapter<AdapterSuperhero.ViewHolderSuperhero>() {
+
+    fun updateData(list:List<Superhero>){
+        mDataset=list
+        notifyDataSetChanged()
+    }
+
+    class ViewHolderSuperhero(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var nameSuperhero : TextView = itemView.name_superhero
+        var idSuperhero: TextView=itemView.id_superhero
+        var imagenSuperhero: ImageView=itemView.image_superhero
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderSuperhero {
+        return ViewHolderSuperhero(LayoutInflater.from(parent.context).inflate(R.layout.superhero_viewholder,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderSuperhero, position: Int) {
+       val hero =mDataset[position]
+        holder.nameSuperhero.text=hero.name
+        holder.idSuperhero.text=hero.id.toString()
+        Picasso.get().load("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/${hero.slug}.jpg").into(holder.imagenSuperhero)
+    }
+
+    override fun getItemCount(): Int {
+        return mDataset.size
+    }
 }
--- app/src/main/java/com/example/superheroeskvh/view/MainActivity.kt
+++ app/src/main/java/com/example/superheroeskvh/view/MainActivity.kt
@@ -7,22 +7,33 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.map
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.superheroeskvh.R
 import com.example.superheroeskvh.model.dataclass.Superhero
 import com.example.superheroeskvh.viewmodel.VMSuperhero
+import kotlinx.android.synthetic.main.activity_main.*
 
 //instancia de recicler, adapter y viewmodel
 class MainActivity : AppCompatActivity() {
 private lateinit var mViewModel:VMSuperhero
     private lateinit var lista:List<Superhero>
+private lateinit var heroAdapter : AdapterSuperhero
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         lista= ArrayList()
         mViewModel=ViewModelProvider(this).get(VMSuperhero::class.java)
-        mViewModel.cargarDatosDeInternetAListaDeDB()
+        mViewModel.cargarDatosDeInternetAListaDeDB() //este paso parece que se puede unir a otro, desde el repositorio
+        heroAdapter=AdapterSuperhero(lista)
+        recycler_superhero.adapter=heroAdapter
+        recycler_superhero.layoutManager=LinearLayoutManager(this)
+        recycler_superhero.addItemDecoration(TopSpacingItemDecoration(30))
+
+
         mViewModel.getListFromDB().observe(this, Observer {
             lista=it
+            heroAdapter.updateData(lista)
+
             Log.d("lista","dentro observer: ${lista}")
         })
         Log.d("lista","fuera del observe: ${lista}")
